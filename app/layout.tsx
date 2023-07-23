91a0bb85678384aa78d3beb3aacdbf6b78923dc4
ahmedshaikhsw <ahmed.shaikh626@gmail.com>
2023-07-23 01:21:14 -0230

    add universal header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Image from 'next/image'
+// Components
+import Header from './Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header 
+          logo={
+            <Image
+              src="/vercel.svg"
+              alt="Froogle Lens Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          }
+          title="Froogle Lens"
+        />
+        {children}
+      </body>
     </html>
   )
 }
