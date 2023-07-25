706249a39f4c139868e5904493381ea3ebcce69a
ahmedshaikhsw <ahmed.shaikh626@gmail.com>
2023-07-25 15:05:03 -0230

    use redis store result, add route for get

--- api/app.py
+++ api/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from PIL import Image
 import io
-import uuid
+import os
+from dotenv import load_dotenv
+import redis
+import json
 
+# Image classification imports
 import tensorflow as tf
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import (
@@ -11,58 +15,104 @@ from tensorflow.keras.applications.mobilenet_v2 import (
     decode_predictions,
 )
 
+# Load environment variables
+load_dotenv()
+
+# Initialize the Redis connection
+r = redis.from_url(os.getenv("REDIS_URL"))
+
 app = Flask(__name__)
 CORS(app)
 
+# Initialize JSON object for storing results
+try:
+    r.execute_command("JSON.SET", "image_classification_results", ".", "{}")
+except Exception as e:
+    print(e)
+
 # Load the pre-trained MobileNetV2 model
 model = MobileNetV2(weights="imagenet")
-# Initialize empty dictionary to store classification results
-# Will replace with a database like Firebase/MongoDB in prod
-classification_results = {}
 
 
-@app.route("/upload", methods=["POST"])
-def upload():
+# Define the /classify route
+@app.route("/classify", methods=["POST"])
+def classify():
     if "image" not in request.files:
         return jsonify({"error": "No image provided."}), 400
 
-    image_file = request.files["image"]
+    try:
+        image_file = request.files["image"]
 
-    # Preprocess the image
-    img = Image.open(io.BytesIO(image_file.read()))
-    img = img.resize((224, 224))
-    img = img.convert("RGB")
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)
-    img_array = preprocess_input(img_array)
+        # Preprocess the image
+        img = Image.open(io.BytesIO(image_file.read()))
+        img = img.resize((224, 224))
+        img = img.convert("RGB")
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+        img_array = preprocess_input(img_array)
 
-    # Make predictions using the pre-trained model
-    predictions = model.predict(img_array)
+        # Make predictions using the pre-trained model
+        predictions = model.predict(img_array)
 
-    # Get top 3 predictions
-    decoded_predictions = decode_predictions(predictions, top=3)[0]
+        # Get top 3 predictions
+        decoded_predictions = decode_predictions(predictions, top=3)[0]
 
-    results = []
-    for _, label, confidence in decoded_predictions:
-        results.append({"label": label, "confidence": float(confidence)})
+        results = {
+            "first": {
+                "label": decoded_predictions[0][1],
+                "confidence": decoded_predictions[0][2] * 100,
+            },
+            "second": {
+                "label": decoded_predictions[1][1],
+                "confidence": decoded_predictions[1][2] * 100,
+            },
+            "third": {
+                "label": decoded_predictions[2][1],
+                "confidence": decoded_predictions[2][2] * 100,
+            },
+        }
 
-    # Generate a unique ID for the classification result
-    result_id = str(uuid.uuid4())
+        # Store the results in Redis
+        r.execute_command(
+            "JSON.SET", "image_classification_results", ".", json.dumps(results)
+        )
+        response_data = {"message": "Image classification result stored in database."}
+        status_code = 200
 
-    # Store the classification result in the dictionary
-    classification_results[result_id] = results
-    print(classification_results)
+    # Error handling
+    except Exception as e:
+        print(e)
+        response_data = {"message": "Error running image classification."}
+        status_code = 500
 
-    return jsonify({"result_id": result_id}), 200
+    # Return success response
+    return Response(
+        json.dumps(response_data), status=status_code, mimetype="application/json"
+    )
 
 
-@app.route("/results/<result_id>", methods=["GET"])
-def get_results(result_id):
-    # Check if the result_id exists in the classification_results dictionary
-    if result_id in classification_results:
-        return jsonify(classification_results[result_id]), 200
-    else:
-        return jsonify({"error": "Result not found."}), 404
+# Define the /results route
+@app.route("/results", methods=["GET"])
+def results():
+    try:
+        # Get the results from Redis
+        results = json.loads(
+            r.execute_command("JSON.GET", "image_classification_results")
+        )
+
+        response_data = results
+        status_code = 200
+
+    # Error handling
+    except Exception as e:
+        print(e)
+        response_data = {"message": "Error getting image classification results."}
+        status_code = 500
+
+    # Return success response
+    return Response(
+        json.dumps(response_data), status=status_code, mimetype="application/json"
+    )
 
 
 if __name__ == "__main__":
