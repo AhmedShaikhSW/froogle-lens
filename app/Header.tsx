8da20642f0a2ac193bfd3ab4390a61bdc3938c13
ahmedshaikhsw <ahmed.shaikh626@gmail.com>
2023-07-26 01:06:12 -0230

    fix navbar elevation

--- app/Header.tsx
+++ app/Header.tsx
@@ -9,7 +9,7 @@ export default function Header({
 }) {
     return (
         <header className="flex items-center justify-center p-4 mt-4">
-            <div className="flex items-center fixed w-screen justify-center space-x-2 p-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/10">
+            <div className="flex items-center fixed w-screen justify-center space-x-2 p-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/10 z-50">
                 <div>{logo}</div>
                 <Link
                     level="h4"
